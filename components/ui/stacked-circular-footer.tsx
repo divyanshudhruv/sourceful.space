db44e9b87b5716e1f045b2610c7b4ed8b3b41b68
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-04-25 16:33:26 +0530

    refactor: remove unused imports from StackedCircularFooter component

--- components/ui/stacked-circular-footer.tsx
+++ components/ui/stacked-circular-footer.tsx
@@ -1,8 +1,3 @@
-import { Icons } from "@/components/ui/icons";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Facebook, Github, Instagram, Linkedin, Twitter } from "lucide-react";
 
 function StackedCircularFooter() {
   return (
